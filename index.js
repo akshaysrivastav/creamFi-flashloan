42539597f00776c0c569c2207ea3794a95e07b3e
akshaysrivastav <akshaysrivastav2014@gmail.com>
2020-09-10 15:43:48 +0530

    removed redundant

--- index.js
+++ index.js
@@ -1,49 +1,9 @@
 
-const { ethers } = require("ethers");
-const HDWalletProvider = require("@truffle/hdwallet-provider");
-require('dotenv').config()
-
-// const { listen } = require('./scripts/yield');
-const addresses = require('./scripts/common/addresses');
-
 process.on('uncaughtException', err => {
   console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
   console.log(err.name, err.message);
   process.exit(1);
 });
-
-const network = process.env.NETWORK || 'kovan';
-
-const web3Provider = new HDWalletProvider(process.env.PRIVATEKEY, `https://${network}.infura.io/v3/${process.env.INFURA_ID}`);
-const provider = new ethers.providers.Web3Provider(web3Provider);
-
-console.log('\nScript Started...');
-
-const contractAddresses = addresses[network];
-
-let world = {};    // global object
-world.provider = provider;
-world.contractAddresses = contractAddresses;
-
-const start = async () => {
-  try {
-
-    let accountAddr = await provider.getSigner().getAddress();
-    let nonce = await provider.getTransactionCount(accountAddr);
-    world.account = {
-      address: accountAddr,
-      nonce: nonce
-    }
-
-    console.log(`\nUsing Account: ${accountAddr}`);
-
-    listen(world)    // for testing
-    // startCron();
-  } catch (error) {
-    console.error(error);
-  }
-}
-start();
 
 process.on('unhandledRejection', err => {
   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
--- truffle-config.js
+++ truffle-config.js
@@ -63,9 +63,9 @@ module.exports = {
 
   compilers: {
     solc: {
-      version: "0.5.16",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      settings: {          // See the solidity docs for advice about optimization and evmVersion
+      version: "0.5.16",
+      // docker: true,
+      settings: {
        optimizer: {
          enabled: true,
          runs: 200
